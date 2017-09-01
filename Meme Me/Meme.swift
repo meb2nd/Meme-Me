566352c53d03752d48d68e7c08bb2b8e29d18915
Pete Barnes <pete_barnes@me.com>
2017-09-01 17:58:15 -0400

    Missed file on last commit.

--- Meme Me/Meme.swift
+++ Meme Me/Meme.swift
@@ -17,38 +17,4 @@ struct Meme {
     var memedImage: UIImage
     var font: String
     
-    // Conveneince method to save a meme
-    static func addToSavedMemes(meme: Meme) {
-
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.memes.append(meme)
-    }
-    
-    // Convenience method to get copy of the saved memes
-    static func getSavedMemes() -> [Meme] {
-        
-        return (UIApplication.shared.delegate as! AppDelegate).memes
-    }
-    
-    // Conveneince method to remove meme from saved collection
-    static func removeAtIndex(_ index: Int) {
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let memes = appDelegate.memes
-        
-        if memes.indices.contains(index) {
-            appDelegate.memes.remove(at: index)
-        }
-        
-    }
-    
-    // Conveneince method to retrieve meme from saved collection
-    static func getMemeAtIndex(_ index: Int) -> Meme? {
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let memes = appDelegate.memes
-        
-        return memes.indices.contains(index) ? memes[index] : nil
-    }
-    
 }
